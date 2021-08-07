2aebc3fef9e601fbfee0274f6576b61bcaa7f96f
m7md-arwani <moh.3ara@hotmail.com>
2021-08-07 21:05:21 +0300

    Adding comments. Making the code more readable.

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,36 +3,42 @@ package machine;
 import java.util.Scanner;
 import java.lang.Math;
 
+// This is the client class where the program starts.
 public class CoffeeMachine {
 
 
     public static void main(String[] args) {
-        while (state.isRunning) {
-            state.programRuns(state.currentState);
-        }
 
-
+        state.start();
     }
 
 
 }
 
-
+// This class will maintain the state of the coffee machine. No calculations.
 class state {
     public static Scanner scanner = new Scanner(System.in);
-    public static boolean isRunning = true;
-    public static State currentState = State.CHOOSING_AN_ACTION;
+    public static boolean isRunning = true; // an indicator of whether the program is running or not.
+    public static State currentState = State.CHOOSING_AN_ACTION; // the default state is to choose an action.
 
-    enum State {
+    enum State { // the enum is helping in organizing the states of the machine.
         CHOOSING_AN_ACTION, CHOOSING_A_COFFEE, FILLING_MACHINE, TAKING_MONEY
     }
 
-    public static void programRuns(State state) {
+    public static void start() { // This function starts the machine.
+        while (state.isRunning) {
+            state.operationsManager(state.currentState);
+        }
+
+
+    }
+
+    public static void operationsManager(State state) { // This function will operate differently according to the current state.
         switch (state) {
             case CHOOSING_AN_ACTION:
                 System.out.println("Write action (buy, fill, take, remaining, exit):");
                 String action = scanner.nextLine();
-                switch (action) {
+                switch (action) { // this nested switch is meant to change the state.
                     case "buy":
                         currentState = State.CHOOSING_A_COFFEE;
                         break;
@@ -50,6 +56,7 @@ class state {
                         break;
                 }
                 break;
+            // From here, the cases will act according to the state that have been changed in the previous switch.
             case CHOOSING_A_COFFEE:
                 newCoffeeMachine.buy();
                 currentState = State.CHOOSING_AN_ACTION;
@@ -66,15 +73,16 @@ class state {
     }
 }
 
-
+// This class will handle the different required calculations.
 class newCoffeeMachine {
+    // these variables are the initial resources of the machine.
     static double water = 400;
     static double milk = 540;
     static double coffeeBeans = 120;
     static int cups = 9;
     static int money = 550;
 
-
+    // The enum is organizing the invariants costs of each coffee type.
     enum coffTypes {
         ESPRESSO(250, 0, 16, 4), LATTE(350, 75, 20, 7),
         CAPPUCCINO(200, 100, 12, 6),
@@ -84,6 +92,7 @@ class newCoffeeMachine {
         int coffeeBeans;
         int money;
 
+        // the constructor of the enum.
         coffTypes(int water, int milk, int coffeeBeans, int money) {
             this.water = water;
             this.milk = milk;
@@ -100,7 +109,7 @@ class newCoffeeMachine {
         System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
         Scanner g = new Scanner(System.in);
         String choice = g.nextLine();
-        switch (choice) {
+        switch (choice) { // Depending on the choice of the user, the required amount of anything will differ.
             case "1":
                 if (check_before_selling(coffTypes.ESPRESSO.water, coffTypes.ESPRESSO.milk, coffTypes.ESPRESSO.coffeeBeans, 1)) {
                     water -= coffTypes.ESPRESSO.water;
@@ -140,6 +149,7 @@ class newCoffeeMachine {
 
     }
 
+    // This function fills resources.
     public static void fill() {
         Scanner g = new Scanner(System.in);
         System.out.println("Write how many ml of water you want to add:");
@@ -153,6 +163,7 @@ class newCoffeeMachine {
 
     }
 
+    // This function checks before doing any deductions from the resources to make sure the machine has the sufficient amount.
     public static boolean check_before_selling(int neededWater, int neededMilk, int neededCoffee, int quantity) {
 
         if (!(Math.floor(water / (neededWater * quantity)) >= quantity)) {
@@ -178,6 +189,7 @@ class newCoffeeMachine {
         return true;
     }
 
+    // This function empties the machine from money
     public static void take() {
         System.out.println("I gave you " + money);
         money = 0;
@@ -185,6 +197,7 @@ class newCoffeeMachine {
 
     }
 
+    // Returns back the current amount of resources.
     public static void status() {
         System.out.println("The coffee machine has:");
         System.out.println((int) water + " ml of water");
